ddadaaa608bc3c843af53c992283881cb0a58591
iappa pedroza <itstecnologiacriativa@gmail.com>
2022-04-29 17:05:13 -0300

    PokeNext - Um visualizador de Pokemon simples - Next JS

--- pages/pokemon/[pokemonId].js
+++ pages/pokemon/[pokemonId].js
@@ -12,7 +12,7 @@ export const getStaticPaths = async () => {
 
   const paths = data.results.map((pokemon, index) => {
     return {
-      params: { pokemonId: index.toString() },
+      params: { pokemonId: (index + 1).toString() },
     };
   });
 
